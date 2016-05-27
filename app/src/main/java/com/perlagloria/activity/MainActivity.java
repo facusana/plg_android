0a14119a7731359d869c6ba9368c9efea84bc714
Alexander Kovalchuk <fatality174@gmail.com>
2016-05-27 22:21:28 +0300

    added "Powered by"

--- app/src/main/java/com/perlagloria/activity/MainActivity.java
+++ app/src/main/java/com/perlagloria/activity/MainActivity.java
@@ -6,15 +6,26 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.perlagloria.R;
 import com.perlagloria.util.AppController;
 import com.perlagloria.util.ErrorAlertDialog;
+import com.perlagloria.util.FontManager;
+import com.perlagloria.util.ServerApi;
 import com.perlagloria.util.SharedPreferenceKey;
 
 import org.json.JSONException;
@@ -22,28 +33,65 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOADING_TEST_TAG = "loading_team_test";
+    private LinearLayout poweredByContainer;
+    private TextView poweredByTitle;
+    private ImageView champLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
 
+        poweredByContainer = (LinearLayout) findViewById(R.id.powered_by_container);
+        poweredByTitle = (TextView) findViewById(R.id.powered_by_title);
+        champLogo = (ImageView) findViewById(R.id.champ_logo);
+
         SharedPreferences sPref = getSharedPreferences("config", Context.MODE_PRIVATE);
         final int savedTeamId = sPref.getInt(SharedPreferenceKey.TEAM_ID, -1);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = null;
-                if (savedTeamId == -1) {    //if team was never selected
-                    intent = new Intent(getApplicationContext(), ChooseTeamActivity.class);
-                } else {    //check if team is still available on server
-                    intent = new Intent(getApplicationContext(), TeamActivity.class);    //if team is available -> move to last screen
+        if (savedTeamId != -1) {
+            poweredByTitle.setTypeface(FontManager.getInstance().getFont(FontManager.Fonts.HELVETICA_NEUE_LIGHT, MainActivity.this));
+            Glide.with(champLogo.getContext())
+                    .load(ServerApi.loadCustomerImageByTeamIdUrl + savedTeamId)
+                    .thumbnail(0.5f)
+                    .crossFade()
+                    .fitCenter()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .listener(new RequestListener<String, GlideDrawable>() {
+                        @Override
+                        public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                            poweredByContainer.setVisibility(View.VISIBLE);
+
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Intent intent = new Intent(getApplicationContext(), TeamActivity.class);    //if team is available -> move to last screen
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }, 2000);   //show splashscreen during 2 sec
+
+                            return false;
+                        }
+                    })
+                    .into(champLogo);
+        } else {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(getApplicationContext(), ChooseTeamActivity.class);
+                    startActivity(intent);
+                    finish();
                 }
-                startActivity(intent);
-                finish();
-            }
-        }, 2000);   //show splashscreen during 2 sec
+            }, 2000);   //show splashscreen during 2 sec
+        }
+
+
 
 
         //checkIsDataFromServerJObject(ServerApi.loadFixtureMatchInfoUrl + savedTeamid);
